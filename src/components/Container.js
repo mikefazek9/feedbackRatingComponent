519bb5a96c3543ac96323ae7bd3680d38897901d
unknown <mike.fazek@gmail.com>
2022-04-26 21:29:54 -0500

    first!

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import IconStar from "../assets/images/icon-star.svg";
+import Text from "./Text"
+function Container() {
+  return (
+    <div>
+     <img src={IconStar} alt="IconStar" />
+     <Text/>
+    </div>
+  )
+}
+
+export default Container
---
+++ src/components/Text.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <div className='text-section'>
+        <h1>How Did we do?</h1>
+        <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+    </div>
+  )
+}
+
+export default Text
